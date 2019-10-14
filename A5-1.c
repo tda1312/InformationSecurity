cbea085c55a536116409fbe9d16bbd88b22a5973
tda1312 <tda1312@users.noreply.github.com>
2019-10-14 12:15:47 +0700

    update algorithm

--- A5-1.c
+++ A5-1.c
@@ -11,12 +11,12 @@
 
 #define BITS 10 // number of key stream bits
 
-struct Registers{
+typedef struct Registers{
     int x[SIZE_X];
     int y[SIZE_Y];
     int z[SIZE_Z];
     int output[BITS];
-} regs;
+} Regs;
 
 int maj(int x, int y, int z){
     int num_of_zeros, num_of_ones;
@@ -37,7 +37,7 @@ int maj(int x, int y, int z){
 }
 
 void shift(struct Registers *regsPointer, int size){
-    int input;
+    int input, j;
 
     if(size == SIZE_X){
         input = regsPointer->x[SIZE_X - 1] ^ regsPointer->x[SIZE_X - 2] ^ regsPointer->x[SIZE_X - 3] ^ regsPointer->x[SIZE_X - 6];
@@ -47,43 +47,79 @@ void shift(struct Registers *regsPointer, int size){
         input = regsPointer->z[SIZE_Z - 1] ^ regsPointer->z[SIZE_Z - 2] ^ regsPointer->z[SIZE_Z - 3] ^ regsPointer->z[SIZE_Z - 15];
     }
 
+	for(j = (size - 1); j >= 0; j--) {
+		if(size == SIZE_X) {
+			if(j == 0) {
+				regsPointer->x[j] = input;
+			}
+			else{
+				regsPointer->x[j] = regsPointer->x[j - 1];
+			}
+		}
+		else if(size == SIZE_Y) {
+			if (j == 0) {
+				regsPointer->y[j] = input;
+			}
+			else {
+				regsPointer->y[j] = regsPointer->y[j - 1];
+			}
+		}
+	}
 }
 
-void generate(){
-
+void generate(struct Registers *registers, int outvalue){
+	int i;
+	
+	printf("\nKey stream: ");
+	for (i = outvalue; i <= BITS; i++) {
+		printf("%d, ", registers->output[i]);
+	}
 }
 
-void display(struct Registers *registers, int size){
-
+void print_menu(){
+	printf("\n--------------------------MENU--------------------------\n");
+	printf("0. Exit the program\n");
+	printf("1. A5/1 Key generation algorithm\n");
 }
 
 int main(){
     //assigning values
-    regs.x = {1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
-    regs.y = {1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1};
-    regs.z = {1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0};
-    struct Registers *regsPointer = &regs;
+    Regs regs = {   {1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+					{1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1},
+					{1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0}   };
+	int i, choice;
+	Regs *regsPointer = &regs;
+	
+	print_menu();
+	printf("Enter your choice: ");
+	scanf("%d", &choice);
+	switch(choice){
+		case 0:
+			break;
+		case 1:
+				
+			for(i = 0; i < BITS; i++){
+				// compare
+				int m = maj(regs.x[CLOCK_X], regs.y[CLOCK_Y], regs.z[CLOCK_Z]);
 
-    for(int i = 0; i < BITS; i++){
-        // compare
-        int m = maj(regs.x[CLOCK_X], regs.y[CLOCK_Y], reg.z[CLOCK_Z]);
+       			//shifting
+       			if(regs.x[CLOCK_X] == m){
+           			shift(regsPointer, SIZE_X);
+       			}
+       			if(regs.y[CLOCK_Y] == m){
+           			shift(regsPointer, SIZE_Y);
+       			}
+       			if(regs.z[CLOCK_Z] == m){
+           			shift(regsPointer, SIZE_Z);
+       			}
 
-        //shifting
-        if(regs.x[CLOCK_X] == m){
-            shift(regs, SIZE_X);
-        }
-        if(regs.y[CLOCK_Y] == m){
-            shift(regs, SIZE_Y);
-        }
-        if(regs.z[CLOCK_Z] == m){
-            shift(regs, SIZE_Z);
-        }
-
-        regsPointer->output[bits - i] = regs.x[SIZE_X - i] ^ regs.y[SIZE_Y - i] ^ regs.z[SIZE_X - i];
-        generate(regsPointer);
+       			regsPointer->output[BITS - i] = regs.x[SIZE_X - i] ^ regs.y[SIZE_Y - i] ^ regs.z[SIZE_X - i];
+       			generate(regsPointer, BITS-i);
+       		}
+        		
+       		break;
     }
-
-    display(regsPointer, BITS);
-
+	
+	
     return 0;
 }
